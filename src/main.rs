ae8a161e6303e13f39b2afa99b32adaed9b3dee0
isaaguilar <isa@aguilarstory.com>
2025-04-26 15:08:01 -0700

    cache update

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ struct AppState {
 
 impl AppState {
     async fn get_current_slot(&self) -> Result<u64, Box<dyn std::error::Error + Send + Sync>> {
-        // client.get_slot_with_commitment(CommitmentConfig { commitment: CommitmentLevel::Confirmed })
         self.client.get_slot().await.map_err(|e| e.into())
     }
 
@@ -44,8 +43,8 @@ impl AppState {
         let current_slot = self.get_current_slot().await?;
         msg!("Current slot: {}", current_slot);
 
-        let mut write_cache = self.cached.write().await;
-        let mut last_updated_index = self.last_updated_index.write().await;
+        let write_cache = self.cached.read().await;
+        let last_updated_index = self.last_updated_index.read().await;
 
         let last_written_slot = if *last_updated_index == 0 {
             write_cache[BUF_SIZE - 1]
@@ -67,6 +66,18 @@ impl AppState {
             .get_confirmed_blocks_between_slots(start_slot, Some(current_slot))
             .await?;
 
+        drop(last_updated_index);
+        drop(write_cache);
+
+        self.update_cache(blocks).await;
+
+        Ok(())
+    }
+
+    async fn update_cache(&self, blocks: Vec<u64>) {
+        let mut write_cache = self.cached.write().await;
+        let mut last_updated_index = self.last_updated_index.write().await;
+
         for block in blocks {
             write_cache[*last_updated_index] = block;
             if *last_updated_index == BUF_SIZE - 1 {
@@ -79,60 +90,8 @@ impl AppState {
         println!("{:?}", *write_cache);
         drop(last_updated_index);
         drop(write_cache);
-
-        Ok(())
     }
 }
-
-// async fn check_blocks(
-//     state: Arc<AppState>,
-// ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-//     let last_updated_index = state.last_updated_index.read().await;
-//     msg!("Last updated index: {:?}", last_updated_index);
-//     drop(last_updated_index);
-
-//     // client.get_slot_with_commitment(CommitmentConfig { commitment: CommitmentLevel::Confirmed })
-//     let current_slot = state.get_current_slot().await?;
-//     msg!("Current slot: {}", current_slot);
-
-//     let mut write_cache = state.cached.write().await;
-//     let mut last_updated_index = state.last_updated_index.write().await;
-
-//     let last_written_slot = if *last_updated_index == 0 {
-//         write_cache[BUF_SIZE - 1]
-//     } else {
-//         write_cache[*last_updated_index - 1]
-//     };
-
-//     msg!("Current index: {}", last_updated_index);
-
-//     let start_slot = if last_written_slot == 0 {
-//         current_slot
-//     } else {
-//         last_written_slot.min(current_slot)
-//     };
-
-//     msg!("Last written slot: {}", last_written_slot);
-
-//     let blocks = state
-//         .get_confirmed_blocks_between_slots(start_slot, Some(current_slot))
-//         .await?;
-
-//     for block in blocks {
-//         write_cache[*last_updated_index] = block;
-//         if *last_updated_index == BUF_SIZE - 1 {
-//             *last_updated_index = 0
-//         } else {
-//             *last_updated_index += 1;
-//         }
-//     }
-
-//     println!("{:?}", *write_cache);
-//     drop(last_updated_index);
-//     drop(write_cache);
-
-//     Ok(())
-// }
 
 async fn bg_cache_syncer(
     state: Arc<AppState>,
@@ -145,12 +104,13 @@ async fn bg_cache_syncer(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // tracing_subscriber::fmt::init();
-    let rpc_url = "https://api.mainnet-beta.solana.com";
     // TODO find out why my api key is not working
-    // let rpc_url = "https://solana-mainnet.api.syndica.io/api-key/28AgWgrGWVHNyMHQZGcMZbTHKHra8J44sWNMJ8KQnfzuZra6pCGMx8CHDWGtvTwZ7B6UuYviwnKykXLdFe1ea4nXQpKp2EbJM82"; // https://solana-mainnet.api.syndica.io/api-key/28AgWgrGWVHNyMHQZGcMZbTHKHra8J44sWNMJ8KQnfzuZra6pCGMx8CHDWGtvTwZ7B6UuYviwnKykXLdFe1ea4nXQpKp2EbJM82
+    let syndica_api_url = std::env::var("SYNDICA_API_URL")
+        .unwrap_or(String::from("https://api.mainnet-beta.solana.com"));
 
-    let client = RpcClient::new(rpc_url.to_string());
+    // tracing_subscriber::fmt::init();
+
+    let client = RpcClient::new(syndica_api_url);
 
     let state = Arc::new(AppState {
         client: client,
@@ -162,7 +122,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::task::spawn(async move { bg_cache_syncer(cache_state).await });
 
     let app = Router::new()
-        .route("/isSlotConfirmed/{slot}", get(get_something))
+        .route("/isSlotConfirmed/{slot}", get(get_is_slot_confirmed))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -171,8 +131,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-// basic handler that responds with a static string
-async fn get_something(
+async fn get_is_slot_confirmed(
     Path(slot): Path<u64>,
     State(state): State<Arc<AppState>>,
 ) -> Result<(StatusCode, String), (StatusCode, String)> {
@@ -191,42 +150,17 @@ async fn get_something(
             .map_err(|e| {
                 (
                     StatusCode::NOT_ACCEPTABLE,
-                    String::from("Failed to get blocks"),
+                    format!("Failed to get blocks: {:?}", e),
                 )
             })?;
-    } else {
-        return Ok((StatusCode::OK, slot.to_string()));
-    }
 
-    let ro_cached = state.cached.read().await;
-    let cached = ro_cached.clone();
-    drop(ro_cached);
-    if !cached.contains(&slot) {
-        Ok((StatusCode::OK, slot.to_string()))
+        if blocks.len() > 0 {
+            state.update_cache(blocks).await;
+            Ok((StatusCode::OK, slot.to_string()))
+        } else {
+            Err((StatusCode::NOT_FOUND, String::new()))
+        }
     } else {
-        Err((StatusCode::NOT_FOUND, String::new()))
+        Ok((StatusCode::OK, slot.to_string()))
     }
 }
-
-/*
-
-// msg!("Length of cache is {}", cache.iter().len());
-
-    // client.get_slot_with_commitment(CommitmentConfig { commitment: CommitmentLevel::Confirmed })
-    let current_slot = match state.client.get_slot().await {
-        Ok(slot) => slot,
-        Err(e) => return Err((StatusCode::OK, format!("Error fetching slot: {:?}", e))),
-    };
-
-    msg!("Current slot: {}", current_slot);
-
-    match state
-        .client
-        .get_blocks(current_slot - 10, Some(current_slot))
-        .await
-    {
-        Ok(blocks) => Ok((StatusCode::OK, format!("Latest blocks: {:?}", blocks))),
-        Err(e) => Err((StatusCode::OK, format!("Error fetching slot: {:?}", e))),
-    }
-
-*/
